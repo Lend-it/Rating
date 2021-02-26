653e279e6a1e438650b704d44f55809bd2787186
Lucas Dutra <ldutra98@gmail.com>
2021-02-26 19:17:38 -0300

    build: Fit Flask environment to docker configs

--- project/__init__.py
+++ project/__init__.py
@@ -1,5 +1,14 @@
 import os
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from database_singleton import Singleton
+
+@app.route('/users/ping', methods=['GET'])
+def ping_pong():
+    return jsonify({
+        'status': 'success',
+        'message': 'pong!'
+    })
 
 
 # instantiate the app
@@ -10,5 +19,15 @@ def create_app(script_info=None):
     # Set Configuration
     app_settings = os.getenv('APP_SETTINGS')
     app.config.from_object(app_settings)
+
+    db = Singleton().database_connection()
+    migrate = Singleton().migration()
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    @app.shell_context_processor
+    def ctx():
+        return {'app': app, 'db': db}
 
     return app
