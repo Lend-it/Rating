05094edb85be19cd1d283e6469affc1bb064175e
Lucas Dutra <ldutra98@gmail.com>
2021-02-26 19:18:18 -0300

    build: Create tests boiler plate

---
+++ project/tests/base.py
@@ -0,0 +1,19 @@
+from flask_testing import TestCase
+from project import create_app
+from database_singleton import Singleton
+
+db = Singleton().database_connection()
+app = create_app()
+
+class BaseTestCase(TestCase):
+    def create_app(self):
+        app.config.from_object('project.config.TestingConfig')
+        return app
+
+    def setUp(self):
+        db.create_all()
+        db.session.commit()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
