22cdeda0c0d8e501fcc832e31a01968ca3551c43
Lucas Dutra <ldutra98@gmail.com>
2021-03-05 10:43:13 -0300

    build: Add database postgres script

--- project/db/create.sql
+++
@@ -1,3 +0,0 @@
-CREATE DATABASE rating_prod;
-CREATE DATABASE rating_dev;
-CREATE DATABASE rating_test;
---
+++ project/db/create_test.sql
@@ -0,0 +1,38 @@
+-- ------------------------     << RateLendItDB - V1 >>     ------------------------
+-- 
+--                    SCRIPT DE CRIACAO (DDL)
+-- 
+-- Data Criacao ...........: 03/03/2021
+-- Autor(es) ..............: Rogério Júnior
+-- Banco de Dados .........: PostgreSQL
+-- Banco de Dados(nome) ...: ratelenditdb
+-- 
+-- PROJETO => 01 Base de Dados
+--         => 04 Tabelas
+-- ------------------------------------------------------------------------------------
+
+CREATE DATABASE rateLendItDB_test
+    WITH
+        ENCODING = UTF8
+        LC_COLLATE = 'pt_BR.UTF-8'
+        LC_CTYPE = 'pt_BR.UTF-8'
+        TEMPLATE = template0;
+
+\c ratelenditdb_test
+
+CREATE TABLE RATE (
+    rateId UUID NOT NULL,
+    stars REAL NOT NULL,
+    review TEXT NOT NULL,
+    repport BOOLEAN NULL,
+    reviewed TEXT NOT NULL,
+    reviewer TEXT NOT NULL,
+
+    CONSTRAINT RATE_PK PRIMARY KEY (rateId),
+
+    CONSTRAINT VALID_REVIEWED_EMAIL CHECK (reviewed ~* '^[\w\-\.]+@([\w-]+\.)+[\w-]{2,4}$'),
+    CONSTRAINT VALID_REVIEWER_EMAIL CHECK (reviewer ~* '^[\w\-\.]+@([\w-]+\.)+[\w-]{2,4}$'),
+    CONSTRAINT VALID_LENDER_USER CHECK (reviewed <> reviewer),
+
+    CONSTRAINT VALID_STARS CHECK (stars BETWEEN 0 AND 5)
+);
