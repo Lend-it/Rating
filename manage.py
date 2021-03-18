b22c2a41940f03d35b3c33f876b753f5e62e0bdc
Lucas Dutra <ldutra98@gmail.com>
2021-03-18 11:21:30 -0300

    docs: fga-eps-mds/2020.2-Lend.it#101 Add COV make commando to readme

--- manage.py
+++ manage.py
@@ -2,11 +2,14 @@ import coverage
 
 COV = coverage.coverage(
     branch=True,
-    include='project/*',
+    include="project/*",
     omit=[
-        'project/tests/*',
-        'project/config.py'
-    ]
+        "project/tests/*",
+        "project/config.py",
+        "project/api/models.py",
+        "project/api/__init__.py",
+        "project/__init__.py",
+    ],
 )
 COV.start()
 
@@ -39,21 +42,21 @@ def test():
         return 0
     return 1
 
+
 @cli.command()
 def cov():
     """Runs the unit tests with coverage."""
-    tests = unittest.TestLoader().discover('project/tests')
+    tests = unittest.TestLoader().discover("project/tests")
     result = unittest.TextTestRunner(verbosity=2).run(tests)
     if result.wasSuccessful():
         COV.stop()
         COV.save()
-        print('Coverage Summary:')
+        print("Coverage Summary:")
         COV.report()
         COV.html_report()
         COV.xml_report()
         return 0
     return 1
-
 
 
 if __name__ == "__main__":
